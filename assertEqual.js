9982d4b36f7d6e3158d14934bd774e8706bd9a80
Tyler <t.mar92@gmail.com>
2019-09-16 17:08:31 +0000

    a funner assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  let wrong = String.fromCodePoint(0x274c);
+  let right = String.fromCharCode(0x2705);
+  if (actual === expected) {
+    
+    console.log(right + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(wrong + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("yes", "yes");
+assertEqual(2,5);

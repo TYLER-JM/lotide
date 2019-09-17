74983649fd9272a181517cbca5302a67779abe8a
Tyler <t.mar92@gmail.com>
2019-09-17 22:19:43 +0000

    a function that finds all the indices of each letter in a string

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  } else {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(a, b) {
+  let wrong = String.fromCodePoint(0x274c);
+  let right = String.fromCodePoint(0x2705);
+  if (eqArrays(a, b)) {
+    return `${right} Assertion Passed, Arrays Equal: ${a} | ${b}`;
+  } else {
+    return `${wrong} Assertion Failed, Arrays Not Equal: ${a} | ${b}`;
+  }
+};
+
+const letterPositions = function(sentence) {
+  let final = {};
+  let noSpace = sentence.split(" ").join("").toLowerCase();
+
+  for (let i = 0; i < noSpace.length; i++) {
+    if (final[noSpace[i]]) {
+      final[noSpace[i]].push(i);
+    } else {
+      final[noSpace[i]] = [i];
+    }
+  }
+
+  return final;
+};
+
+console.log(assertArraysEqual(letterPositions("hello").l , [2,3]));
+console.log(assertArraysEqual(letterPositions("johnJOHN").j , [0,4]));
+console.log(assertArraysEqual(letterPositions("john TOM AlleN").n , [3,11]));

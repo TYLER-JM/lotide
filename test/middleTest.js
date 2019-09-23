1d383cfb53adda8b826efb61dbbef15c3bc6b233
Tyler <t.mar92@gmail.com>
2019-09-23 13:02:32 +0000

    using mocha chai tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,17 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-// TEST CODE
-console.log(assertArraysEqual(middle([1,2]), []));
-console.log(assertArraysEqual(middle([1,2,3]), [2]));
-console.log(assertArraysEqual(middle([1,2,3,4]), [2, 3]));
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]));
+describe("#middle", () => {
+  it("returns and empty array...", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it("returns [2]...", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("returns [2,3]...", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it("returns ['john']...", () => {
+    assert.deepEqual(middle(["bill", "john", "greg"]), ["john"]);
+  });
+});

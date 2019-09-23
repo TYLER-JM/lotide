6dff1e4fc7928f667eb0b9cf63da774dbb4f1ca6
Tyler <t.mar92@gmail.com>
2019-09-23 12:36:00 +0000

    using module.exports and require

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(first, second) {
-  if (first.length !== second.length) {
-    return false;
-  } else {
-    for (let i = 0; i < first.length; i++) {
-      if (first[i] !== second[i]) return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(a, b) {
-  let wrong = String.fromCodePoint(0x274c);
-  let right = String.fromCodePoint(0x2705);
-  if (eqArrays(a, b)) {
-    return `${right} Assertion Passed, Arrays Equal: ${a} | ${b}`;
-  } else {
-    return `${wrong} Assertion Failed, Arrays Not Equal: ${a} | ${b}`;
-  }
-};
-
 const middle = function(arr) {
   let middleArr = [];
   let midPoint = Math.ceil(arr.length / 2) - 1;
@@ -33,8 +12,4 @@ const middle = function(arr) {
   return middleArr;
 };
 
-// TEST CODE
-console.log(assertArraysEqual(middle([1,2]), []));
-console.log(assertArraysEqual(middle([1,2,3]), [2]));
-console.log(assertArraysEqual(middle([1,2,3,4]), [2, 3]));
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]));
+module.exports = middle;

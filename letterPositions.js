b01cfb4f1883a5757381bda46de17b5e1374373b
Tyler <t.mar92@gmail.com>
2019-09-23 17:49:08 +0000

    removed commented-out code

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,3 @@
-// const eqArrays = function(first, second) {
-//   if (first.length !== second.length) {
-//     return false;
-//   } else {
-//     for (let i = 0; i < first.length; i++) {
-//       if (first[i] !== second[i]) return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(a, b) {
-//   let wrong = String.fromCodePoint(0x274c);
-//   let right = String.fromCodePoint(0x2705);
-//   if (eqArrays(a, b)) {
-//     return `${right} Assertion Passed, Arrays Equal: ${a} | ${b}`;
-//   } else {
-//     return `${wrong} Assertion Failed, Arrays Not Equal: ${a} | ${b}`;
-//   }
-// };
 
 const letterPositions = function(sentence) {
   let final = {};

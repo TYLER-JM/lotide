58f9fb7bb4d3c26325b2bab41332dc39805cd2d9
Tyler <t.mar92@gmail.com>
2019-09-23 16:37:42 +0000

    implemented mocha testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,25 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
+
+describe("#middle", () => {
+  it("length should be 3...", () => {
+    assert.deepEqual(tail([2,4,6,8]).length, 3);
+  });
+  it("returns [2, 3]...", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3]);
+  });
+  it("returns [2,3,4]...", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3, 4]);
+  });
+  it("returns ['john', 'greg']...", () => {
+    assert.deepEqual(tail(["bill", "john", "greg"]), ["john", "greg"]);
+  });
+});
 
 //TEST CODE
-let testArray = [2,4,6,8];
-let singleArray = [];
-console.log(tail(singleArray));
-console.log(assertEqual(tail(testArray).length, 3));
-console.log(testArray, testArray.length);
+// let testArray = [2,4,6,8];
+// let singleArray = [];
+// console.log(tail(singleArray));
+// console.log(assertEqual(tail(testArray).length, 3));
+// console.log(testArray, testArray.length);

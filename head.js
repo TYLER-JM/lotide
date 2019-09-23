90ab38f1523c32d5d8cb599184d646c4561a8d71
Tyler <t.mar92@gmail.com>
2019-09-23 12:15:10 +0000

    added module.exports

--- head.js
+++ head.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let wrong = String.fromCodePoint(0x274c);
-  let right = String.fromCharCode(0x2705);
-  if (actual === expected) {
-    
-    //console.log(right + " Assertion Passed: " + actual + " === " + expected);
-    console.log(`${right} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    //console.log(wrong + " Assertion Failed: " + actual + " !== " + expected);
-    console.log(`${wrong} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(arr) {
   return arr[0];
 };
 
-//TESTING CODE
-console.log(assertEqual(head([1,2,3]), 1));
-console.log(assertEqual(head(["hello","goodbye","adeos"]), "hello"));
+module.exports = head;
